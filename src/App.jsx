137b6992f501c7fd02c6159f3fb31c27a2070964
Ariel Mattos <arielmattosnev@gmail.com>
2023-05-31 14:01:58 -0300

    feat: finished app component

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./styles/Global.css";
+
+import { Box } from "@mui/material";
+
+import { Route, Routes } from "react-router-dom";
+
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Box width="400px" sx={{ width: { xl: "1448px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+}
+
+export default App;
